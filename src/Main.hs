c9acc8c095b58a5ea359e2a7b89654416c5ca3eb
Diogo Castro <dcastro@spotx.tv>
2018-04-17 09:32:57 +0100

    Repro

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import           Control.Concurrent.Async
+import           Control.Monad
+import           Data.Text
+import           Network.HTTP.Types
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Network.Wai.Handler.WebSockets
+import           Network.WebSockets
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  run 3000 app
+
+app :: Application
+app = websocketsOr defaultConnectionOptions wsApp backupApp
+  where
+    wsApp :: ServerApp
+    wsApp pending_conn = do
+        conn <- acceptRequest pending_conn
+        forever $ do
+          race_
+           (receiveData conn :: IO Text)
+           (pure ())
+          putStrLn "Retrying"
+
+    backupApp :: Application
+    backupApp _ respond = respond $ responseLBS status400 [] "Not a WebSocket request"
+    
